704dd9ab17cd64acdb170b23553dd4a7b3a62d15
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-08-23 22:56:47 +0300

    ~ Switch to simplified AbstractList implementation from GWT.

--- js/js.libraries/src/core/collections/AbstractList.kt
+++ js/js.libraries/src/core/collections/AbstractList.kt
@@ -13,6 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Based on GWT AbstractList
+ * Copyright 2007 Google Inc.
+*/
+
 
 package kotlin.collections
 
@@ -20,9 +25,11 @@ public abstract class AbstractList<E> protected constructor() : AbstractCollecti
     abstract override val size: Int
     abstract override fun get(index: Int): E
 
-    override fun set(index: Int, element: E): E = throw UnsupportedOperationException()
-    override fun add(index: Int, element: E): Unit = throw UnsupportedOperationException()
-    override fun removeAt(index: Int): E = throw UnsupportedOperationException()
+    protected var modCount: Int = 0
+
+    override fun add(index: Int, element: E): Unit = throw UnsupportedOperationException("Add not supported on this list")
+    override fun removeAt(index: Int): E = throw UnsupportedOperationException("Remove not supported on this list")
+    override fun set(index: Int, element: E): E = throw UnsupportedOperationException("Set not supported on this list")
 
     override fun add(element: E): Boolean {
         add(size, element)
@@ -30,316 +37,212 @@ public abstract class AbstractList<E> protected constructor() : AbstractCollecti
     }
 
     override fun addAll(index: Int, elements: Collection<E>): Boolean {
-        var modified = false
         var index = index
-        for (element in elements) {
-            add(index++, element)
-            modified = true
+        var changed = false
+        for (e in elements) {
+            add(index++, e)
+            changed = true
         }
-        return modified
+        return changed
     }
 
     override fun clear() {
         removeRange(0, size)
     }
 
-    protected open fun removeRange(fromIndex: Int, toIndex: Int) {
+
+    override fun iterator(): MutableIterator<E> = IteratorImpl()
+
+    override fun indexOf(element: E): Int {
+        for (i in 0..lastIndex) {
+            if (get(i) == element) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    override fun lastIndexOf(element: E): Int {
+        for (i in lastIndex downTo 0) {
+            if (get(i) == element) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    override fun listIterator(): MutableListIterator<E> = listIterator(0)
+    override fun listIterator(from: Int): MutableListIterator<E> = ListIteratorImpl(from)
+
+
+    override fun subList(fromIndex: Int, toIndex: Int): MutableList<E> = SubList(this, fromIndex, toIndex)
+
+    protected open fun removeRange(fromIndex: Int, endIndex: Int) {
         val iterator = listIterator(fromIndex)
-        repeat(toIndex - fromIndex) {
+        for (i in fromIndex..endIndex - 1) {
             iterator.next()
             iterator.remove()
         }
     }
 
-    override fun indexOf(element: E): Int {
-        val iterator = listIterator()
-        while (iterator.hasNext())
-            if (iterator.next() == element)
-                return iterator.previousIndex()
-        return -1
-    }
+    override fun equals(other: Any?): Boolean {
+        if (other === this) return true
+        if (other !is List<*>) return false
+        if (size != other.size) return false
 
-    override fun lastIndexOf(element: E): Int {
-        val iterator = listIterator(size)
-        while (iterator.hasPrevious())
-            if (iterator.previous() == element)
-                return iterator.nextIndex()
-        return -1
-    }
-
-    override fun iterator(): MutableIterator<E> = ListIterator(0)
-    override fun listIterator(): MutableListIterator<E> = ListIterator(0)
-    override fun listIterator(index: Int): MutableListIterator<E> {
-        if (index !in 0..size) {
-            throw IndexOutOfBoundsException("Index: $index, size: $size");
+        val otherIterator = other.iterator()
+        for (elem in this) {
+            val elemOther = otherIterator.next()
+            if (elem != elemOther) {
+                return false
+            }
         }
-        return ListIterator(index)
+
+        return true
     }
-
-    override fun subList(fromIndex: Int, toIndex: Int): MutableList<E> = SubList(this, fromIndex, toIndex)
-
 
     override fun hashCode(): Int {
-        var hashCode = 1
-        for (element in this)
-            hashCode = 31 * hashCode + (element?.hashCode() ?: 0)
-        return hashCode
-
+        return Collections_hashCode(this)
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (other === this)
-            return true
-        if (other !is List<*>)
-            return false
 
-        val iterator1 = listIterator()
-        val iterator2 = other.listIterator()
-        while (iterator1.hasNext() && iterator2.hasNext()) {
-            val element1 = iterator1.next()
-            val element2 = iterator2.next()
-            if (element1 != element2)
-                return false
-        }
-        return !(iterator1.hasNext() || iterator2.hasNext())
-    }
+    private open inner class IteratorImpl : MutableIterator<E> {
+        /*
+     * i is the index of the item that will be returned on the next call to
+     * next() last is the index of the item that was returned on the previous
+     * call to next() or previous (for ListIterator), -1 if no such item exists.
+     */
 
-    protected var modCount: Int = 0
+        internal var i = 0
+        internal var last = -1
 
-    private inner open class ListIterator(private var cursor: Int) : MutableListIterator<E> {
-        private var modificationIndex = -1
-        private var expectedModCount = modCount
-
-        override fun hasNext() = cursor < size
-        override fun hasPrevious() = cursor > 0
-        override fun nextIndex(): Int = cursor
-        override fun previousIndex(): Int = cursor - 1
+        override fun hasNext(): Boolean = i < size
 
         override fun next(): E {
-            checkForComodification()
-            try {
-                return get(cursor).apply { modificationIndex = cursor++ }
-            }
-            catch (e: IndexOutOfBoundsException) {
-                checkForComodification()
-                throw NoSuchElementException()
-            }
+            if (!hasNext()) throw NoSuchElementException()
+            last = i++
+            return get(last)
         }
 
         override fun remove() {
-            if (modificationIndex == -1)
-                throw IllegalStateException()
-            checkForComodification()
+            check(last != -1)
 
-            try {
-                this@AbstractList.removeAt(modificationIndex)
-                if (modificationIndex < cursor)
-                    cursor--
-                modificationIndex = -1
-                expectedModCount = modCount
-            }
-            catch (e: IndexOutOfBoundsException) {
-                throw ConcurrentModificationException()
-            }
-        }
-
-        private fun checkForComodification() {
-            if (modCount != expectedModCount)
-                throw ConcurrentModificationException()
-        }
-
-
-        override fun previous(): E {
-            checkForComodification()
-            try {
-                val nextCursor = cursor - 1
-                return get(nextCursor).apply {
-                    cursor = nextCursor
-                    modificationIndex = nextCursor
-                }
-            }
-            catch (e: IndexOutOfBoundsException) {
-                checkForComodification()
-                throw NoSuchElementException()
-            }
-        }
-
-
-        override fun set(e: E) {
-            if (modificationIndex == -1)
-                throw IllegalStateException()
-            checkForComodification()
-
-            try {
-                this@AbstractList[modificationIndex] = e
-                expectedModCount = modCount
-            }
-            catch (ex: IndexOutOfBoundsException) {
-                throw ConcurrentModificationException()
-            }
-        }
-
-        override fun add(e: E) {
-            checkForComodification()
-
-            try {
-                this@AbstractList.add(cursor++, e)
-                modificationIndex = -1
-                expectedModCount = modCount
-            }
-            catch (ex: IndexOutOfBoundsException) {
-                throw ConcurrentModificationException()
-            }
+            removeAt(last)
+            i = last
+            last = -1
         }
     }
 
-    private class SubList<E>(val list: AbstractList<E>, fromIndex: Int, toIndex: Int) : AbstractList<E>() {
+    /**
+     * Implementation of `ListIterator` for abstract lists.
+     */
+    private inner class ListIteratorImpl(start: Int) : IteratorImpl(), MutableListIterator<E> {
+        /*
+     * i is the index of the item that will be returned on the next call to
+     * next() last is the index of the item that was returned on the previous
+     * call to next() or previous (for ListIterator), -1 if no such item exists.
+     */
+
         init {
-            if (fromIndex < 0)
-                throw IndexOutOfBoundsException("fromIndex = $fromIndex")
-            if (toIndex > list.size)
-                throw IndexOutOfBoundsException("toIndex = $toIndex")
-            if (fromIndex > toIndex)
-                throw IllegalArgumentException("fromIndex($fromIndex) > toIndex($toIndex)")
+            checkPositionIndex(start, this@AbstractList.size)
+
+            i = start
         }
-        private val offset = fromIndex
-        private var _size = toIndex - fromIndex
-        private var expectedModCount = list.modCount
 
-        override val size: Int
-            get() = _size.apply { checkForComodification() }
+        override fun add(o: E) {
+            add(i, o)
+            i++
+            last = -1
+        }
 
-        override fun set(index: Int, element: E): E {
-            rangeCheck(index)
-            checkForComodification()
-            return list.set(index + offset, element)
+        override fun hasPrevious(): Boolean = i > 0
+
+        override fun nextIndex(): Int = i
+
+        override fun previous(): E {
+            if (!hasPrevious()) throw NoSuchElementException()
+
+            last = --i
+            return get(last)
+        }
+
+        override fun previousIndex(): Int = i - 1
+
+        override fun set(o: E) {
+            require(last != -1)
+
+            this@AbstractList[last] = o
+        }
+    }
+
+    private class SubList<E>(private val wrapped: AbstractList<E>, private val fromIndex: Int, toIndex: Int) : AbstractList<E>() {
+        private var _size: Int = 0
+
+        init {
+            checkCriticalPositionIndexes(fromIndex, toIndex, wrapped.size)
+            this._size = toIndex - fromIndex
+        }
+
+        override fun add(index: Int, element: E) {
+            checkPositionIndex(index, _size)
+
+            wrapped.add(fromIndex + index, element)
+            _size++
         }
 
         override fun get(index: Int): E {
-            rangeCheck(index)
-            checkForComodification()
-            return list.get(index + offset)
+            checkElementIndex(index, _size)
+
+            return wrapped[fromIndex + index]
         }
-
-
-        override fun add(index: Int, element: E) {
-            insertionRangeCheck(index)
-            checkForComodification()
-            list.add(index + offset, element)
-            expectedModCount = list.modCount
-            _size++
-            this.modCount++
-        }
-
 
         override fun removeAt(index: Int): E {
-            rangeCheck(index)
-            checkForComodification()
-            val result = list.removeAt(index + offset)
-            expectedModCount = list.modCount
+            checkElementIndex(index, _size)
+
+            val result = wrapped.removeAt(fromIndex + index)
             _size--
-            this.modCount++
             return result
         }
 
-        override fun removeRange(fromIndex: Int, toIndex: Int) {
-            checkForComodification()
-            list.removeRange(fromIndex + offset, toIndex + offset)
-            expectedModCount = list.modCount
-            _size -= toIndex - fromIndex
-            modCount++
+        override fun set(index: Int, element: E): E {
+            checkElementIndex(index, _size)
+
+            return wrapped.set(fromIndex + index, element)
         }
 
-        override fun addAll(c: Collection<E>): Boolean = addAll(_size, c)
+        override val size: Int get() = _size
+    }
 
-        override fun addAll(index: Int, elements: Collection<E>): Boolean {
-            insertionRangeCheck(index)
-            val otherSize = elements.size
-            if (otherSize == 0)
-                return false
-
-            checkForComodification()
-            list.addAll(offset + index, elements)
-            expectedModCount = list.modCount
-            _size += otherSize
-            modCount++
-            return true
-        }
-
-        override fun iterator(): MutableIterator<E> = listIterator()
-
-        override fun listIterator(index: Int): MutableListIterator<E> {
-            checkForComodification()
-            insertionRangeCheck(index)
-
-            return object : MutableListIterator<E> {
-                private val iterator = list.listIterator(index + offset)
-
-                override operator fun hasNext(): Boolean {
-                    return nextIndex() < _size
-                }
-
-                override operator fun next(): E {
-                    if (hasNext())
-                        return iterator.next()
-                    else
-                        throw NoSuchElementException()
-                }
-
-                override fun hasPrevious(): Boolean {
-                    return previousIndex() >= 0
-                }
-
-                override fun previous(): E {
-                    if (hasPrevious())
-                        return iterator.previous()
-                    else
-                        throw NoSuchElementException()
-                }
-
-                override fun nextIndex(): Int {
-                    return iterator.nextIndex() - offset
-                }
-
-                override fun previousIndex(): Int {
-                    return iterator.previousIndex() - offset
-                }
-
-                override fun remove() {
-                    iterator.remove()
-                    expectedModCount = list.modCount
-                    _size--
-                    modCount++
-                }
-
-                override fun set(e: E) {
-                    iterator.set(e)
-                }
-
-                override fun add(e: E) {
-                    iterator.add(e)
-                    expectedModCount = list.modCount
-                    _size++
-                    modCount++
-                }
+    companion object {
+        internal fun checkElementIndex(index: Int, size: Int) {
+            if (index < 0 || index >= size) {
+                throw IndexOutOfBoundsException("Index: $index, Size: $size")
             }
         }
 
-
-        private fun rangeCheck(index: Int) {
-            if (index !in 0.._size-1)
+        internal fun checkPositionIndex(index: Int, size: Int) {
+            if (index < 0 || index > size) {
                 throw IndexOutOfBoundsException("Index: $index, Size: $size")
+            }
         }
 
-        private fun insertionRangeCheck(index: Int) {
-            if (index !in 0.._size)
-                throw IndexOutOfBoundsException("Index: $index, Size: $size")
+        internal fun checkCriticalPositionIndexes(start: Int, end: Int, size: Int) {
+            if (start < 0 || end > size) {
+                throw IndexOutOfBoundsException("fromIndex: $start, toIndex: $end, size: $size")
+            }
+            if (start > end) {
+                throw IllegalArgumentException("fromIndex: $start > toIndex: $end")
+            }
         }
 
-        private fun checkForComodification() {
-            if (list.modCount != expectedModCount)
-                throw ConcurrentModificationException()
+        internal fun <T> Collections_hashCode(list: List<T>): Int {
+            var hashCode = 1
+            for (e in list) {
+                hashCode = 31 * hashCode + (e?.hashCode() ?: 0)
+                hashCode = hashCode or 0 // make sure we don't overflow
+            }
+            return hashCode
         }
     }
-}
 
+}
